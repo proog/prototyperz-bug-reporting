27b06e985b29fd950ec706a6c5b3bf82c633c0e9
Per Mortensen <per@permortensen.com>
2017-04-09 18:48:33 +0200

    Added reports controller skeleton

--- frontendAdmin/build.js
+++ frontendAdmin/build.js
@@ -1,15 +1,29 @@
-var app = angular.module('insightApp', ['ngRoute', 'route-segment', 'view-segment', 'ngAnimate', 'angular-loading-bar', 'ngSanitize', 'dropzone', 'wu.masonry', 'ngResource', 'ui.bootstrap', 'angular-jqcloud']);
+var app = angular.module('ReportBackApp', ['ngRoute', 'route-segment', 'view-segment', 'ngAnimate', 'ngSanitize', 'ngResource', 'ui.bootstrap']);
 /// <reference path="../app.ts" />
 var LoginController = (function () {
     function LoginController(http) {
         this.http = http;
     }
-    LoginController.prototype.hej = function (u) {
-        this.http.get("user").then(function (response) {
-            var asdasd = response.data;
-        });
-    };
     return LoginController;
 }());
 LoginController.$inject = ["$http"];
 app.controller("loginController", LoginController);
+/// <reference path="../app.ts" />
+var ReportController = (function () {
+    function ReportController(http, q) {
+        this.http = http;
+        this.q = q;
+        this.baseUrl = "https://permortensen.com/bugs";
+        this.projectId = "58ea0efc40a27fe61a55179a";
+    }
+    ReportController.prototype.getReports = function () {
+        var _this = this;
+        this.http.get(this.baseUrl + "/projects/" + this.projectId + "/reports")
+            .then(function (response) {
+            _this.reports = response.data;
+        });
+    };
+    return ReportController;
+}());
+ReportController.$inject = ["$http", "$q"];
+app.controller("reportController", ReportController);
